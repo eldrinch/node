deee7cbd5fc4bfd10fdb9288a6e075b8dec3c64a
eldrinch <eldrinc@hotmail.com>
2021-10-29 01:50:52 -0300

    Routing, Build Simple API, Building the templates, Filling the templates

--- firststep/index.js
+++ firststep/index.js
@@ -1,11 +1,12 @@
-//visit and read documentation https://nodejs.org
+//visit and read documentation https://nomoduledejs.org
 
 // const hello = 'Hello world';
 // console.log(hello);
 
 //Bloking, synchronous way
-const fs = require('fs');
-const http = require('http')
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 // const textIn =  fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn);
@@ -34,15 +35,68 @@ const http = require('http')
 // });
 // console.log(`Will read file!`);
 
-//############ 11. CREATING A SIMPLE WEBSERVER ############
+//############ 11, 12 CREATING A SIMPLE WEBSERVER ############
 
-const server = http.createServer((req, res)=>{
-  console.log(req);
-  res.end('Hello from the server!')
-})
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nitrients);
+  output = output.replace(/{%QUANTITY%}/g, product.image);
+  output = output.replace(/{%DESCRIPTION%}/g, product.image);
+  output = output.replace(/{%ID%}/g, product.image);
 
-server.listen(8000, '127.0.0.1', () => {
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+
+  //Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    //generate a new array
+    const cardsHtml = dataObj.map((el) => {return replaceTemplate(tempCard, el);}).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+    res.end(output);
+
+    //Product page
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+
+    //API
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err, data)=>{
+    //   const productData = JSON.parse(data);
+    //   res.writeHead(200,{'Content-type': 'application/json'})
+    //   // console.log(productData);
+    //   res.end(data);
+    // })
+
+    //Not found
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-own-header": "Hello World",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+  // res.end('Hello from the server!')
+});
+
+server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
-})
-
-
+});
---
+++ firststep/index_zero.js
@@ -0,0 +1,73 @@
+//visit and read documentation https://nomoduledejs.org
+
+// const hello = 'Hello world';
+// console.log(hello);
+
+//Bloking, synchronous way
+const fs = require('fs');
+const http = require('http')
+const url = require('url')
+
+// const textIn =  fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn);
+
+// const textOut = `This is what we know about abocado: ${textIn}. \n Created on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written');
+
+//Non-bloking, asynchronous way
+
+//############ 10 READING AND WRITING FILES ASNCHRONOUSLY ############
+
+// fs.readFile('./txt/start.txt','utf8', (err,data1)=>{
+//   if(err) return console.log('ERROR!');
+
+//   fs.readFile(`./txt/${data1}.txt`,'utf8', (err,data2)=>{
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`,'utf8', (err,data3)=>{
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err =>{
+//         console.log('Your file has been written');
+//       })
+//     });
+//   });
+// });
+// console.log(`Will read file!`);
+
+//############ 11, 12 CREATING A SIMPLE WEBSERVER ############
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
+  
+
+
+const server = http.createServer((req, res)=>{
+  console.log(req.url);
+  const pathName = req.url;
+  if(pathName ===  '/'|| pathName === '/overview'){
+    res.end('This is a OVERVIEW')
+  }else if (pathName === '/product'){
+    res.end('This is the PRODUCT')
+  }else if (pathName === '/api'){
+    res.writeHead(200,{'Content-type': 'application/json'})
+    res.end(data);
+    // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err, data)=>{
+    //   const productData = JSON.parse(data);
+    //   res.writeHead(200,{'Content-type': 'application/json'})
+    //   // console.log(productData);
+    //   res.end(data);
+    // })
+  }else{
+    res.writeHead(404, { 
+      'Content-Type': 'text/html',
+      'my-own-header': 'Hello World'
+    })
+    res.end("<h1>Page not found!</h1>")
+  }
+ // res.end('Hello from the server!')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log("Listening to request on port 8000");
+})
